a18c92416758fb2c388ffbb788c40629ddeb4dd1
Fortyseven <fortyseven@gmail.com>
2015-08-21 02:01:41 -0400

    Added original NeaveTV video collection

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
 var tv = null;
 
 var clip_list = [
-//    "content/video/weird_dog.mp4",
-    "content/video/yt/liesontheinternet.webm"
+    "content/video/neavetv/weird_dog.mp4"
+    , "content/video/neavetv/adhisaya_piravi.mp4"
+    , "content/video/neavetv/aerobic.mp4"
+    , "content/video/neavetv/armpit_hair.mp4"
+    , "content/video/neavetv/bells_dubstep.mp4"
+    , "content/video/neavetv/bim_bam_boom.mp4"
+    , "content/video/neavetv/bomb.mp4"
+    , "content/video/neavetv/broken.mp4"
+    , "content/video/neavetv/carnival.mp4"
+    , "content/video/neavetv/club_of_friends.mp4"
+    , "content/video/neavetv/dark_star.mp4"
+    , "content/video/neavetv/death_koala.mp4"
+    , "content/video/neavetv/devil_girl.mp4"
+    , "content/video/neavetv/eegah.mp4"
+    , "content/video/neavetv/erogenous_zone.mp4"
+    , "content/video/neavetv/for_your_height_only.mp4"
+    , "content/video/neavetv/germs.mp4"
+    , "content/video/neavetv/godzilla_flying_kick.mp4"
+    , "content/video/neavetv/hand_dj.mp4"
+    , "content/video/neavetv/happy_xmas.mp4"
+    , "content/video/neavetv/hello_dave.mp4"
+    , "content/video/neavetv/helvetica_scenario.mp4"
+    , "content/video/neavetv/hungry.mp4"
+    , "content/video/neavetv/internal_conflict.mp4"
+    , "content/video/neavetv/jacko_cat.mp4"
+    , "content/video/neavetv/kkssee.mp4"
+    , "content/video/neavetv/laptop_drop.mp4"
+    , "content/video/neavetv/late_for_meeting.mp4"
+    , "content/video/neavetv/laughing_owl.mp4"
+    , "content/video/neavetv/lets_get_it_on.mp4"
+    , "content/video/neavetv/lost_babies.mp4"
+    , "content/video/neavetv/massive_viral_outbreak.mp4"
+    , "content/video/neavetv/motorcycle.mp4"
+    , "content/video/neavetv/noodle_monster.mp4"
+    , "content/video/neavetv/pizza.mp4"
+    , "content/video/neavetv/prank_call.mp4"
+    , "content/video/neavetv/ronald_mcdonald.mp4"
+    , "content/video/neavetv/skifcha.mp4"
+    , "content/video/neavetv/someone_here.mp4"
+    , "content/video/neavetv/sonar_dog.mp4"
+    , "content/video/neavetv/spoon.mp4"
+    , "content/video/neavetv/squirrel_monster.mp4"
+    , "content/video/neavetv/street_monster.mp4"
+    , "content/video/neavetv/streetlamp.mp4"
+    , "content/video/neavetv/useless_superpower.mp4"
+    , "content/video/neavetv/wolf_spirit.mp4"
+
+    , "content/video/yt/liesontheinternet.webm"
     , "content/video/yt/noice.webm"
     , "content/video/yt/kebab.webm"
     , "content/video/yt/murder.webm"
@@ -62,12 +108,6 @@ var clip_list = [
     , "content/video/yt/ballsohard.webm"
     , "content/video/yt/eagleman.webm"
     , "content/video/yt/2spooky.webm"
-
-
-    //"http://v14.lscache3.c.youtube.com/videoplayback?sparams=id%2Cexpire%2Cip%2Cipbits%2Citag%2Cratebypass%2Coc%3AU0hQRVVRV19FSkNOMF9NS0FD&amp;itag=43&amp;ip=0.0.0.0&amp;signature=60D26B708E92E1636CA3B34A389B1A4406DF290F.BD57D8B601A61EFF68CEF6726754DAD6D9C7C2A9&amp;sver=3&amp;ratebypass=yes&amp;expire=1310569200&amp;key=yt1&amp;ipbits=0&amp;id=885f37c308a3f36f"
-    //"http://r7---sn-p5qlsnlk.googlevideo.com/videoplayback?ratebypass=yes&ip=2a03:8180:1001:16a::8ee1&mm=31&source=youtube&mn=sn-p5qlsnlk&id=o-ABG1CqwxnB0wPLDvZivnseRZQ9P3ylJj_hcrT9W5ZQ_F&ms=au&mt=1440096890&pl=40&mv=m&mime=video/webm&dur=0.000&upn=Rc0dPdOmQC8&itag=43&ipbits=0&nh=IgpwcjAzLmlhZDA3KgkxMjcuMC4wLjE&fexp=9405265,9407016,9408509,9408710,9409069,9412774,9415365,9415485,9416023,9416126,9416687,9416838,9417117,9417280,9417707,9417712,9418153,9418199,9418223,9418393,9418449&sver=3&signature=23ED2F7F7F45BF1B26D4F7D4A8E2B4D239DFC63F.6A95D914028EC9518F64BDF4321248D83F1F21EB&key=yt5&initcwndbps=1870000&sparams=dur,id,initcwndbps,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,nh,pl,ratebypass,source,upn,expire&expire=1440118631&lmt=1297937138435273",
-    //"http://r1---sn-p5qlsnly.googlevideo.com/videoplayback?mime=video/webm&key=yt5&expire=1440118539&lmt=1298115949986690&sver=3&ipbits=0&itag=43&initcwndbps=1870000&pl=40&dur=0.000&ratebypass=yes&source=youtube&nh=IgpwcjAzLmlhZDA3KgkxMjcuMC4wLjE&id=o-AF_RY9-mGzB_aLp25QH6FMBvaMxfGH9bQog98J40W_qP&ip=2a03:8180:1001:16a::8ee1&sparams=dur,id,initcwndbps,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,nh,pl,ratebypass,source,upn,expire&fexp=9406511,9407178,9407991,9408503,9408710,9409069,9412774,9412839,9415365,9415417,9415485,9416023,9416126,9416231,9416324,9417707,9418153,9418449,9418465,9418901,9419341,9419444,9419675,9419831&ms=au&signature=B8DD6CF04CC50A7C9885AA74FBBE31FAD46491.7B6D29BFC73A89A260ABA28A47DBD78E8DADDCD7&mt=1440096890&mv=m&mm=31&mn=sn-p5qlsnly&upn=FPPJuavZgcI",
-    //"http://r4---sn-p5qlsnll.googlevideo.com/videoplayback?mime=video/webm&id=o-AJT1kvuyHzTWnxDgja5-_fWcMlrX3-8ha0mRi5J8dc1P&pl=40&mm=31&mn=sn-p5qlsnll&ipbits=0&ip=2a03:8180:1001:16a::8ee1&ms=au&mt=1440096003&mv=m&dur=0.000&initcwndbps=2067500&expire=1440117735&sver=3&fexp=9407700,9407991,9408710,9409069,9409207,9415365,9415485,9415555,9415822,9416023,9416126,9416729,9416838,9417203,9417223,9417448,9417689,9417707,9418153,9418200,9418449,9418901,9419299,9419767&sparams=dur,id,initcwndbps,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,nh,pl,ratebypass,source,upn,expire&nh=IgpwcjAzLmlhZDA3KgkxMjcuMC4wLjE&source=youtube&ratebypass=yes&signature=64BA13C2FD337A29844255B35EE5A4A1C47FF47C.3E08B48D15A71B4A184E862E7988192CED663B01&upn=eCqZaboQkzY&itag=43&key=yt5&lmt=1297056240067091"
 ];
 
 var TV = function ( video_selector, interstitial_selector ) {
@@ -155,7 +195,6 @@ var TV = function ( video_selector, interstitial_selector ) {
         _next_interstitial = false;
         _screen[0].pause();
         _interstitial[0].currentTime = 0;
-        _interstitial[0].loop = true;
         _interstitial[0].play();
         _interstitial.show();
         _interstitial_start_time = new Date().getTime();
@@ -172,7 +211,7 @@ var TV = function ( video_selector, interstitial_selector ) {
     }
 
     /**
-     *
+     * When we're playing an interstitial, this will
      */
     this.onClipFinished = function () {
         if ( _next_interstitial == true ) {
